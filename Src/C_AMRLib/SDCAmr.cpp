2eb446e63099c4e56fc016f50824227834c72e95
Matthew Emmett <memmett@gmail.com>
2013-08-22 13:49:32 -0700

    SDCAMR: Level dependent encapsulations.

--- Src/C_AMRLib/SDCAmr.cpp
+++ Src/C_AMRLib/SDCAmr.cpp
@@ -3,8 +3,6 @@
 #include <MultiFab.H>
 #include <ParmParse.H>
 
-#include <cassert>
-
 #include <stdio.h>
 
 using namespace std;
@@ -29,6 +27,14 @@ void SDCAmr::timeStep (int  level,
                          int  niter,
                          Real stop_time)
 {
+  // if (sweepers[0] == NULL) {
+  //   build_encap(0);
+  //   sweepers[0] = sdc_level_bld(*this, 0);
+  //   sdc_mg_add_level(&mg, sweepers[0], mlsdc_amr_interpolate, mlsdc_amr_restrict);
+  //   sdc_mg_setup(&mg);
+  //   sdc_mg_allocate(&mg);
+  // }
+
   // assert(level == 0);
 
   // XXX
@@ -39,16 +45,35 @@ void SDCAmr::timeStep (int  level,
 
   cout << "!!! USING SDC TIME STEP !!!" << endl;
 
-  if (sweepers.size() == 0) {
-    sweepers.push_back(sdc_level_bld(*this, 0));
-    sdc_mg_add_level(&mg, sweepers[0], mlsdc_amr_interpolate, mlsdc_amr_restrict);
-    sdc_mg_setup(&mg);
-    sdc_mg_allocate(&mg);
-  }
-
   Amr::timeStep(level, time, iteration, niter, stop_time);
 }
 
+void SDCAmr::regrid (int  lbase,
+		     Real time,
+		     bool initial)
+{
+  sdc_mg_reset(&mg);
+  // for (unsigned int i=0; i<=finest_level; i++) {
+  //   if (sweepers[i] != NULL) {
+  //     printf("%d %p %p\n", i, sweepers[i], sweepers[i]->destroy);
+  //     sweepers[i]->destroy(sweepers[i]);
+  //     delete (mf_encap_t*) encaps[i]->ctx;
+  //     delete encaps[i];
+  //     sweepers[i] = NULL;
+  //   }
+  // }
+
+  Amr::regrid(lbase, time, initial);
+
+  for (int lev=0; lev<=finest_level; lev++) {
+    encaps[lev]   = build_encap(lev);
+    sweepers[lev] = sdc_level_bld(*this, lev);
+    sdc_mg_add_level(&mg, sweepers[lev], mlsdc_amr_interpolate, mlsdc_amr_restrict);
+  }
+  sdc_mg_setup(&mg);
+  sdc_mg_allocate(&mg);
+  cout << "done" << endl;
+}
 
 SDCAmr::SDCAmr (sdc_level_bld_f bld)
 {
@@ -57,18 +82,13 @@ SDCAmr::SDCAmr (sdc_level_bld_f bld)
   if (!ppsdc.query("max_trefs", max_trefs)) max_trefs = 3;
 
   sdc_level_bld = bld;
-  
-  //
-  // build MultiFab encapsulation for SDCLib
-  //
-  encap_ctx.ncomp = 1;
-  encap_ctx.ngrow = 1;
-  build_encap();
-
-  //
-  // build multigrid sdc sweeper, add coarsest level
-  //
   sdc_mg_build(&mg, max_level);
+
+  sweepers.resize(max_level);
+  encaps.resize(max_level);
+
+  for (unsigned int i=0; i<max_level; i++)
+    sweepers[i] = NULL;
 }
 
 
--- Src/C_AMRLib/SDCAmrEncap.cpp
+++ Src/C_AMRLib/SDCAmrEncap.cpp
@@ -1,21 +1,26 @@
 
 #include <MultiFab.H>
 #include <SDCAmr.H>
+#include <StateDescriptor.H>
+#include <AmrLevel.H>
+
+#include <cassert>
 
 BEGIN_EXTERN_C
 
 void *mf_encap_create(int type, void *encap_ctx)
 {
-  throw "SDCLib should not call mf_encap_create.";
-  // mf_encap_t* ctx = (mf_encap_t*) encap_ctx;
-  // return new MultiFab(ctx->ba, ctx->ncomp, ctx->ngrow);
+  // throw "SDCLib should not call mf_encap_create.";
+  mf_encap_t* ctx = (mf_encap_t*) encap_ctx;
+  cout << ctx->ba->numPts() << endl;
+  return new MultiFab(*ctx->ba, ctx->ncomp, ctx->ngrow);
 }
 
 void mf_encap_destroy(void *sol)
 {
-  throw "SDCLib should not call mf_encap_destroy.";
-  // MultiFab* mf = (MultiFab*) sol;
-  // delete mf;
+  // throw "SDCLib should not call mf_encap_destroy.";
+  MultiFab* mf = (MultiFab*) sol;
+  delete mf;
 }
 
 void mf_encap_setval(void *sol, sdc_dtype_t val)
@@ -49,12 +54,24 @@ void mf_encap_saxpy(void *yp, sdc_dtype_t a, void *xp)
 END_EXTERN_C
 
 
-void SDCAmr::build_encap()
+sdc_encap_t* SDCAmr::build_encap(int lev)
 {
-  encap.create  = mf_encap_create;
-  encap.destroy = mf_encap_destroy;
-  encap.setval  = mf_encap_setval;
-  encap.copy    = mf_encap_copy;
-  encap.saxpy   = mf_encap_saxpy;
-  encap.ctx     = &encap_ctx;
+  const DescriptorList& dl = getLevel(lev).get_desc_lst();
+
+  assert(dl.size() == 1);	// XXX
+  
+  mf_encap_t* ctx = new mf_encap_t;
+  ctx->ba    = &boxArray(lev);
+  ctx->ncomp = dl[0].nComp();
+  ctx->ngrow = dl[0].nExtra();
+
+  sdc_encap_t* encap = new sdc_encap_t;
+  encap->create  = mf_encap_create;
+  encap->destroy = mf_encap_destroy;
+  encap->setval  = mf_encap_setval;
+  encap->copy    = mf_encap_copy;
+  encap->saxpy   = mf_encap_saxpy;
+  encap->ctx     = ctx;
+
+  return encap;
 }
