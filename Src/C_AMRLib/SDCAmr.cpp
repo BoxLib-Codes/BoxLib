488414ee201cc1710abbbee44f82ffe3d5c0c663
Matthew Emmett <mwemmett@lbl.gov>
2013-09-04 12:43:43 -0700

    WIP.

--- Src/C_AMRLib/SDCAmr.cpp
+++ Src/C_AMRLib/SDCAmr.cpp
@@ -4,20 +4,67 @@
 #include <ParmParse.H>
 #include <StateDescriptor.H>
 #include <AmrLevel.H>
-
+#include <Interpolater.H>
+#include <FabArray.H>
 #include <stdio.h>
 
 using namespace std;
 
-
 BEGIN_EXTERN_C
 
-void mlsdc_amr_interpolate(void *F, void *G, void *ctxF, void *ctxG)
+/*
+ * Spatial interpolation is done by...
+ */
+void mlsdc_amr_interpolate(void *F, void *G, void *vctxF, void *vctxG)
 {
-  MultiFab& UF = *((MultiFab*) F);
-  MultiFab& UG = *((MultiFab*) G);
-  AmrLevel& levelF = *((AmrLevel*) ctxF);
-  AmrLevel& levelG = *((AmrLevel*) ctxG);
+  sdc_level_ctx* ctxF = (sdc_level_ctx*) vctxF;
+  sdc_level_ctx* ctxG = (sdc_level_ctx*) vctxG;
+  MultiFab& UF        = *((MultiFab*) F);
+  MultiFab& UG        = *((MultiFab*) G);
+  Amr& amr            = *ctxF->amr;
+  AmrLevel& levelF    = amr.getLevel(ctxF->level);
+  AmrLevel& levelG    = amr.getLevel(ctxG->level);
+
+  const DescriptorList& dl = levelF.get_desc_lst();
+  for (int st=0; st<dl.size(); st++) {
+    int ncomp = dl[st].nComp();
+
+    const Array<BCRec>& bcs = dl[st].getBCs();
+
+    Array<BCRec> bcr(ncomp);
+
+    const IntVect& fine_ratio = amr.refRatio(ctxG->level);
+    Interpolater& mapper      = *dl[st].interp();
+
+    const Box& fine_domain = levelF.get_state_data(st).getDomain();
+
+    for (MFIter mfi(UF); mfi.isValid(); ++mfi) { 
+
+      const Box& fine_box = mfi.validbox();
+
+      FArrayBox fine(fine_box);
+      FArrayBox crse(mapper.CoarseBox(fine_box, fine_ratio));
+      for (int j=0; j<UG.size(); j++) crse.copy(UG[j]);
+
+      cout << crse.box() << endl;
+      cout << fine.box() << endl;
+      cout << fine_box << endl;
+      cout << ncomp << endl;
+      // cout << levelG.Geom() << endl;
+      // cout << levelF.Geom() << endl;
+
+      BoxLib::setBC(fine_box, fine_domain, st, 0, ncomp, bcs, bcr);
+
+      const Geometry& crse_geom = levelG.Geom();
+      const Geometry& fine_geom = levelF.Geom();
+
+
+      cout << "interpolating..." << endl;
+      mapper.interp(crse, 0, fine, 0, ncomp,
+                    fine_box, fine_ratio, crse_geom, fine_geom, bcr, st, st);
+      cout << "interpolating... done." << endl;
+    }
+  }
 
 }
 
@@ -43,7 +90,7 @@ void SDCAmr::timeStep (int  level,
     cout << "NOT ON LEVEL 0" << endl;
   }
 
-  // set initial conditions
+  // set intial conditions...
   for (int lev=0; lev<=finest_level; lev++) {
     AmrLevel& level = getLevel(lev);
     const DescriptorList& dl = level.get_desc_lst();
@@ -56,7 +103,7 @@ void SDCAmr::timeStep (int  level,
   }
 
   // spread and iterate
-  sdc_mg_spread(&mg, time, dtLevel(0));
+  sdc_mg_spread(&mg, time, dtLevel(0), 0);
   for (int k=0; k<max_iters; k++) {
     if (verbose > 0 && ParallelDescriptor::IOProcessor())
       std::cout << "MLSDC iteration: " << k << std::endl;
@@ -81,19 +128,25 @@ void SDCAmr::rebuild_mlsdc()
 {
   // reset previous and clear sweepers etc
   sdc_mg_reset(&mg);
-  for (unsigned int i=0; i<=max_level; i++) {
-    if (sweepers[i] != NULL) {
-      sweepers[i]->destroy(sweepers[i]);
-      delete (mf_encap_t*) encaps[i]->ctx;
-      delete encaps[i];
-      sweepers[i] = NULL;
+  for (unsigned int lev=0; lev<=max_level; lev++) {
+    if (sweepers[lev] != NULL) {
+      delete sweepers[lev]->nset->ctx;
+      sweepers[lev]->destroy(sweepers[lev]);
+      delete (mf_encap*) encaps[lev]->ctx;
+      delete encaps[lev];
+      sweepers[lev] = NULL;
     }
   }
 
   // rebuild
   for (int lev=0; lev<=finest_level; lev++) {
+    sdc_level_ctx* ctx = new sdc_level_ctx;
+    ctx->amr   = this;
+    ctx->level = lev;
     encaps[lev]   = build_encap(lev);
-    sweepers[lev] = sdc_level_bld(*this, lev);
+    sweepers[lev] = sdc_sweeper_bld(lev);
+    sweepers[lev]->nset->ctx   = ctx; // XXX: need to free this...
+    sweepers[lev]->nset->encap = encaps[lev];
     sdc_mg_add_level(&mg, sweepers[lev], mlsdc_amr_interpolate, mlsdc_amr_restrict);
   }
   sdc_mg_setup(&mg);
@@ -111,13 +164,13 @@ void SDCAmr::rebuild_mlsdc()
 //   rebuild_mlsdc();
 // }
 
-SDCAmr::SDCAmr (sdc_level_bld_f bld)
+SDCAmr::SDCAmr (sdc_sweeper_bld_f bld)
 {
   ParmParse ppsdc("mlsdc");
   if (!ppsdc.query("max_iters", max_iters)) max_iters = 22;
   if (!ppsdc.query("max_trefs", max_trefs)) max_trefs = 3;
 
-  sdc_level_bld = bld;
+  sdc_sweeper_bld = bld;
   sdc_mg_build(&mg, max_level+1);
 
   sweepers.resize(max_level+1);
--- Src/C_AMRLib/SDCAmrEncap.cpp
+++ Src/C_AMRLib/SDCAmrEncap.cpp
@@ -10,7 +10,7 @@ BEGIN_EXTERN_C
 
 void *mf_encap_create(int type, void *encap_ctx)
 {
-  mf_encap_t* ctx = (mf_encap_t*) encap_ctx;
+  mf_encap* ctx = (mf_encap*) encap_ctx;
   return new MultiFab(*ctx->ba, ctx->ncomp, ctx->ngrow);
 }
 
@@ -20,7 +20,7 @@ void mf_encap_destroy(void *sol)
   delete mf;
 }
 
-void mf_encap_setval(void *sol, sdc_dtype_t val)
+void mf_encap_setval(void *sol, sdc_dtype val)
 {
   MultiFab& mf = *((MultiFab*) sol);
 
@@ -28,7 +28,7 @@ void mf_encap_setval(void *sol, sdc_dtype_t val)
   mf.setVal(val);
 }
 
-void mf_encap_copy(void *dstp, void *srcp)
+void mf_encap_copy(void *dstp, const void *srcp)
 {
   MultiFab& dst = *((MultiFab*) dstp);
   MultiFab& src = *((MultiFab*) srcp);
@@ -38,7 +38,7 @@ void mf_encap_copy(void *dstp, void *srcp)
     dst[mfi].copy(src[mfi]);
 }
 
-void mf_encap_saxpy(void *yp, sdc_dtype_t a, void *xp)
+void mf_encap_saxpy(void *yp, sdc_dtype a, void *xp)
 {
   MultiFab& y = *((MultiFab*) yp);
   MultiFab& x = *((MultiFab*) xp);
@@ -51,18 +51,18 @@ void mf_encap_saxpy(void *yp, sdc_dtype_t a, void *xp)
 END_EXTERN_C
 
 
-sdc_encap_t* SDCAmr::build_encap(int lev)
+sdc_encap* SDCAmr::build_encap(int lev)
 {
   const DescriptorList& dl = getLevel(lev).get_desc_lst();
 
   assert(dl.size() == 1);	// XXX
   
-  mf_encap_t* ctx = new mf_encap_t;
+  mf_encap* ctx = new mf_encap;
   ctx->ba    = &boxArray(lev);
   ctx->ncomp = dl[0].nComp();
   ctx->ngrow = dl[0].nExtra();
 
-  sdc_encap_t* encap = new sdc_encap_t;
+  sdc_encap* encap = new sdc_encap;
   encap->create  = mf_encap_create;
   encap->destroy = mf_encap_destroy;
   encap->setval  = mf_encap_setval;
