da087cfc150e8a8026157f52596ccd0ca057c370
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-12-10 10:58:26 -0800

    more on source path change

--- Tools/Migration/srcpath.sh
+++ Tools/Migration/srcpath.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-OLD_SRC_PATH="Src\/C_AMRLib"
-NEW_SRC_PATH="Src\/Amr"
-find . -path .git -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+OLD_SRC_PATH="Amr"
+NEW_SRC_PATH="Amr"
+echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
+find . -path .git -prune -o -path 'Tools/Migration' -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
 
-OLD_SRC_PATH="Src\/C_AmrCoreLib"
-NEW_SRC_PATH="Src\/AmrCore"
-find . -path .git -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+OLD_SRC_PATH="AmrCore"
+NEW_SRC_PATH="AmrCore"
+echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
+find . -path .git -prune -o -path 'Tools/Migration' -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
 
-OLD_SRC_PATH="Src\/C_BaseLib"
-NEW_SRC_PATH="Src\/Base"
-find . -path .git -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+OLD_SRC_PATH="Base"
+NEW_SRC_PATH="Base"
+echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
+find . -path .git -prune -o -path 'Tools/Migration' -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
 
-OLD_SRC_PATH="Src\/C_BoundaryLib"
-NEW_SRC_PATH="Src\/Boundary"
-find . -path .git -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+OLD_SRC_PATH="Boundary"
+NEW_SRC_PATH="Boundary"
+echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
+find . -path .git -prune -o -path 'Tools/Migration' -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
 
-OLD_SRC_PATH="Src\/C_ParticleLib"
-NEW_SRC_PATH="Src\/Particle"
-find . -path .git -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+OLD_SRC_PATH="Particle"
+NEW_SRC_PATH="Particle"
+echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
+find . -path .git -prune -o -path 'Tools/Migration' -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+
 
