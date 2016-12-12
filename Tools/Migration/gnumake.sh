589d788eb5c5e8b0b2ce843e7f3b66707b2954b8
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-12-12 10:08:40 -0800

    Migration script: remove FCOMP= line from GNUmakefile

--- Tools/Migration/gnumake.sh
+++ Tools/Migration/gnumake.sh
@@ -4,3 +4,6 @@ OLD_SRC_PATH="Tools\/C_mk"
 NEW_SRC_PATH="Tools\/GNUMake"
 echo ${OLD_SRC_PATH}" --> "${NEW_SRC_PATH}
 find . -type d \( -name .git -o -path './Tools/Migration' \) -prune -o -type f -exec grep -Iq . {} \; -exec sed -i 's/'"${OLD_SRC_PATH}"'/'"${NEW_SRC_PATH}"'/g' {} +
+
+echo "Remove 'FCOMP = ' line from GNUmakefile"
+find . -type d \( -name .git -o -path './Tools/Migration' \) -prune -o -type f -name "GNUmakefile" -exec sed -i '/FCOMP\s*=/d' {} +
